d0766b50a302e3c2b0c17fb62320828bc01b3b01
devdasz <dasprasun1234@gmail.com>
2022-09-22 15:44:30 +0530

    Creating first endpoint

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	// Initialize a Fiber application
+	app := fiber.New()
+
+	// Get function to route to / path and an handler function that returns a JSON
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.JSON(&fiber.Map{
+			"data": "Server has began",
+		})
+	})
+
+	// Get function to route to / path and an handler function that returns a JSON
+	app.Listen(":6000")
+}
