f586092e0ca368fd3c0e5db3f80f7b645f50b3ce
devdasz <dasprasun1234@gmail.com>
2022-09-22 16:19:07 +0530

    Mongodb configuration added

---
+++ configs/setup.go
@@ -0,0 +1,45 @@
+package configs
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+// function that first configures the client to use the correct URI and check for errors
+func ConnectDB() *mongo.Client {
+	client, err := mongo.NewClient(options.Client().ApplyURI(EnvMongoURI()))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//   a timeout of 10 seconds we wanted to use when trying to connect
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err = client.Connect(ctx)
+
+	// check if there is an error while connecting to the database and cancel the connection if the connecting period exceeds 10 seconds
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//ping the database
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Connected to MongoDB")
+	return client
+}
+
+// Client instance
+var DB *mongo.Client = ConnectDB()
+
+// getting database collections
+func GetCollection(client *mongo.Client, collectionName string) *mongo.Collection {
+	collection := client.Database("golangAPI").Collection(collectionName)
+	return collection
+}
